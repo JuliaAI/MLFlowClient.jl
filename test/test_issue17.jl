fd452bf5e81a4e15590b36eaa38e8b1fdb9ab8f2
deyandyankov <deyan@dyankov.name>
2021-12-14 12:57:57 +0000

    refactor tests and start to address issue #17

---
+++ test/test_issue17.jl
@@ -0,0 +1,18 @@
+# Addresses https://github.com/JuliaAI/MLFlowClient.jl/issues/17
+include("test_base.jl")
+
+@testset "Issue17" begin
+    @ensuremlf
+
+    mlf_experiment = getorcreateexperiment(mlf, "issue17")
+    mlf_run = createrun(
+        mlf, 
+        mlf_experiment, 
+        tags=[
+            Dict(
+                "key" => "mlflow.runName",
+                "value" => "run_name"
+            )
+        ]
+    )
+end
